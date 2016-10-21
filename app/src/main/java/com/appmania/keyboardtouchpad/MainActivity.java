262d6ffe55d13eecc7aae58e2a17d43cba03bd69
Pedro Lucas <lucasdasilvajunior@yahoo.com.br>
2016-10-21 04:20:04 -0200

    Added fragment transactions

--- app/src/main/java/com/appmania/keyboardtouchpad/MainActivity.java
+++ app/src/main/java/com/appmania/keyboardtouchpad/MainActivity.java
@@ -1,17 +1,19 @@
 package com.appmania.keyboardtouchpad;
 
+import android.os.PersistableBundle;
+import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 
-import com.appmania.keyboardtouchpad.view.SlidingTabLayout;
+import com.appmania.keyboardtouchpad.fragment.KeyboardFragment;
+import com.appmania.keyboardtouchpad.fragment.MouseFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static String BUNDLE_MODE = "BUNDLE_MODE";
 
     private FloatingActionButton switchButton;
     private int mode = 1;
@@ -21,42 +23,53 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+
         switchButton = (FloatingActionButton)findViewById(R.id.btn_mode_switch);
+
+        if(savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().add(R.id.fragment_container, new MouseFragment()).commit();
+        }else{
+            mode = savedInstanceState.getInt(BUNDLE_MODE);
+            if(mode == MODE_MOUSE) {
+                switchButton.setImageResource(R.drawable.ic_mouse);
+            }else{
+                switchButton.setImageResource(R.drawable.ic_keyboard);
+            }
+        }
+
         switchButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Fragment fragment;
                 if(mode == MODE_MOUSE) {
                     mode = MODE_KEYBOARD;
                     switchButton.setImageResource(R.drawable.ic_keyboard);
+                    fragment = (Fragment)new KeyboardFragment();
                 }else{
                     mode = MODE_MOUSE;
                     switchButton.setImageResource(R.drawable.ic_mouse);
+                    fragment = (Fragment)new MouseFragment();
                 }
+
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+                transaction.replace(R.id.fragment_container, fragment);
+                transaction.addToBackStack(null);
+
+                transaction.commit();
+
             }
         });
+
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    public void onSaveInstanceState(Bundle outState, PersistableBundle outPersistentState) {
+        super.onSaveInstanceState(outState, outPersistentState);
+        outState.putInt(BUNDLE_MODE, mode);
     }
 }
